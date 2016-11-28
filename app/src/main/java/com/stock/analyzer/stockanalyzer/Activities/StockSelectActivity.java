7b4c2e5d49c11148432f3a585850641c8f6648fb
Kevin <kejuntong@gmail.com>
2016-11-28 18:01:46 -0500

    search company by symbol

--- app/src/main/java/com/stock/analyzer/stockanalyzer/Activities/StockSelectActivity.java
+++ app/src/main/java/com/stock/analyzer/stockanalyzer/Activities/StockSelectActivity.java
@@ -5,6 +5,11 @@ import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.opencsv.CSVReader;
 import com.stock.analyzer.stockanalyzer.Adapters.StockSelectAdapter;
@@ -25,6 +30,10 @@ public class StockSelectActivity extends Activity {
     RecyclerView mRecyclerView;
     StockSelectAdapter mAdapter;
     ArrayList<Company> allCompanyList;
+    ArrayList<Company> searchResultList;
+
+    EditText searchText;
+    ImageButton cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +44,21 @@ public class StockSelectActivity extends Activity {
 
         loadCompanyList();
 
+        setSearchText();
+
     }
 
     private void initViews(){
+        searchText = (EditText) findViewById(R.id.search_text);
+        cancelButton = (ImageButton) findViewById(R.id.cancel_button);
+
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setVerticalScrollBarEnabled(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(linearLayoutManager);
         allCompanyList = new ArrayList<>();
-        mAdapter = new StockSelectAdapter(this, allCompanyList);
+        searchResultList = new ArrayList<>();
+        mAdapter = new StockSelectAdapter(this, searchResultList);
         mRecyclerView.setAdapter(mAdapter);
     }
 
@@ -64,6 +79,7 @@ public class StockSelectActivity extends Activity {
             while ((line = csvReader.readNext()) != null) {
                 Company school = new Company(line[0], line[1]);
                 allCompanyList.add(school);
+                searchResultList.add(school);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -85,4 +101,42 @@ public class StockSelectActivity extends Activity {
                 e.printStackTrace();
             }
     }
+
+    private void setSearchText(){
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(final Editable s) {
+                String key = s.toString().trim();
+                cancelButton.setVisibility(key.isEmpty() ? View.GONE : View.VISIBLE);
+                searchCompany(key);
+            }
+        });
+    }
+
+    private void searchCompany(String keyWord){
+
+        searchResultList.clear();
+
+        for (Company company : allCompanyList) {
+            if (company.getSymbol().toUpperCase().contains(keyWord.toUpperCase())) {
+                searchResultList.add(company);
+            }
+        }
+
+        if (searchResultList.size() > 0){
+            mAdapter.notifyDataSetChanged();
+        }
+
+    }
+
 }
