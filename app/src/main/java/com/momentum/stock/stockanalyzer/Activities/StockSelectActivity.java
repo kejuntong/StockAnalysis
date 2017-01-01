9206b6d7b1566a76da6392a5b1ae154ac79db982
Kevin Tong <kejuntong@gmail.com>
2017-01-01 14:03:45 -0500

    separate stock search category

--- app/src/main/java/com/momentum/stock/stockanalyzer/Activities/StockSelectActivity.java
+++ app/src/main/java/com/momentum/stock/stockanalyzer/Activities/StockSelectActivity.java
@@ -1,9 +1,13 @@
 package com.momentum.stock.stockanalyzer.Activities;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
@@ -12,6 +16,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.momentum.stock.stockanalyzer.UtilClasses.Constants;
 import com.opencsv.CSVReader;
 import com.momentum.stock.stockanalyzer.Adapters.StockSelectAdapter;
 import com.momentum.stock.stockanalyzer.Models.Company;
@@ -36,6 +43,7 @@ public class StockSelectActivity extends Activity {
     ImageButton wishListButton;
     EditText searchText;
     ImageButton cancelButton;
+    ImageButton menuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +52,9 @@ public class StockSelectActivity extends Activity {
 
         initViews();
 
-        loadCompanyList();
+        setMenuButton();
+
+        loadCompanyList("stock_list_all.csv");
 
         setSearchText();
 
@@ -76,21 +86,71 @@ public class StockSelectActivity extends Activity {
         searchResultList = new ArrayList<>();
         mAdapter = new StockSelectAdapter(this, searchResultList);
         mRecyclerView.setAdapter(mAdapter);
+
+        menuButton = (ImageButton) findViewById(R.id.menu_button);
     }
 
-    private void loadCompanyList() {
+    private void setMenuButton(){
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SharedPreferences sharedPref = getSharedPreferences(Constants.utilSharePreference, Context.MODE_PRIVATE);
+                final SharedPreferences.Editor editor = sharedPref.edit();
+                final int searchCategory = sharedPref.getInt(Constants.searchCategoryKey, -1);
+
+                MaterialDialog.Builder builder = new MaterialDialog.Builder(StockSelectActivity.this)
+                        .title("Search Category")
+                        .titleColorRes(R.color.black)
+                        .items("Toronto Stock Exchange (TSE)", "Nasdaq Stock Market (NASDAQ)", "New York Stock Exchange (NYSE)")
+                        .itemsColorRes(R.color.black)
+                        .widgetColorRes(R.color.blue)
+                        .positiveText("SELECT")
+                        .itemsCallbackSingleChoice(searchCategory, new MaterialDialog.ListCallbackSingleChoice() {
+                            @Override
+                            public boolean onSelection(MaterialDialog dialog, View itemView, int which, CharSequence text) {
+                                editor.putInt(Constants.searchCategoryKey, which);
+                                editor.apply();
+
+                                switch (which){
+                                    case 0:
+                                        loadCompanyList("stock_list_tsx.csv");
+                                        break;
+                                    case 1:
+                                        loadCompanyList("stock_list_nasdaq.csv");
+                                        break;
+                                    case 2:
+                                        loadCompanyList("stock_list_nyse.csv");
+                                        break;
+                                }
+
+                                return true;
+                            }
+                        })
+                        .positiveColorRes(R.color.blue);
+
+                builder.build().show();
+
+            }
+        });
+    }
+
+    private void loadCompanyList(String listName) {
         InputStream csvStream = null;
         InputStreamReader csvStreamReader = null;
         CSVReader csvReader = null;
         AssetManager assetManager = getAssets();
         try {
-            csvStream = assetManager.open("stock_list_1.csv");
+            csvStream = assetManager.open(listName);
             csvStreamReader = new InputStreamReader(csvStream);
             csvReader = new CSVReader(csvStreamReader, ',');
 
             String[] line;
             // throw away the header
 //            csvReader.readNext();
+
+            allCompanyList.clear();
+            searchResultList.clear();
 
             while ((line = csvReader.readNext()) != null) {
                 Company company = new Company(line[0], line[1]);
--- app/src/main/java/com/momentum/stock/stockanalyzer/UtilClasses/Constants.java
+++ app/src/main/java/com/momentum/stock/stockanalyzer/UtilClasses/Constants.java
@@ -7,4 +7,7 @@ public class Constants {
 
     final public static String wishListSharePreference = "wish_list_share_preference";
     final public static String wishListKey = "wish_list";
+
+    final public static String utilSharePreference = "util_share_preference";
+    final public static String searchCategoryKey = "search_catergory";
 }
